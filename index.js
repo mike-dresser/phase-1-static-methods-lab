1375e907410316e01db2cfd0a42b254309f21c21
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-02-16 16:24:23 -0500

    complete

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return `${string[0].toUpperCase()}${string.slice(1)}`;
+  }
+
+  static sanitize(string) {
+    const key = `abcdefghijklmnopqrstuvwxyz1234567890-' `;
+    let result = '';
+    for (let char of string) {
+      if (key.includes(char.toLowerCase())) {
+        result += char;
+      }
+    }
+    return result;
+  }
+
+  static titleize(string) {
+    const exclude = [
+      'the',
+      'a',
+      'an',
+      'but',
+      'of',
+      'and',
+      'for',
+      'at',
+      'by',
+      'from',
+    ];
+    const array = string.split(' ');
+    array[0] = array[0][0].toUpperCase() + array[0].slice(1).toLowerCase();
+    for (let i = 1; i < array.length; i++) {
+      array[i] = array[i].toLowerCase();
+      if (exclude.includes(array[i])) {
+        continue;
+      }
+      array[i] = array[i][0].toUpperCase() + array[i].slice(1);
+    }
+    return array.join(' ');
+  }
 }
